a90a59f7859665e3033455833b4120f1b4b1d742
HanssenGLRP <66432612+HanssenDevs@users.noreply.github.com>
2024-08-25 14:57:53 +0200

    Add: OppgaveG1 Class

---
+++ Fritid/DAT100Lab1/src/OppgaveG1.java
@@ -0,0 +1,22 @@
+
+public class OppgaveG1 {
+
+	public static void main(String[] args) {
+		//a)
+		int a = 12;
+		int b = 15;
+		//b)
+		a = 5;
+		b = 7;
+		
+		//c)
+		System.out.println(a + "\n" + b);
+		
+		//d)
+		a = true;
+		b = false;
+		System.out.println(a + "\n" + b); //Dette gir feilmelding ettersom at Variabel A og B har deklerasjonen som INT. og kan dermed ikke endres til en annen Data Type
+		
+	}
+
+}
