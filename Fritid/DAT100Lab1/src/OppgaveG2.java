e00bf7097fbb2d7c7b75b4d17f80bd106207122a
HanssenGLRP <66432612+HanssenDevs@users.noreply.github.com>
2024-08-25 15:02:12 +0200

    Create OppgaveG2.java

---
+++ Fritid/DAT100Lab1/src/OppgaveG2.java
@@ -0,0 +1,21 @@
+
+public class OppgaveG2 {
+
+	public static void main(String[] args) {
+		int a = 12;
+		int b = 15;
+		a = 8;
+		b = 7;
+		//a)
+		//b)
+		//c) Så blir C = 15 siden a er endret fra 5 til 8
+		int c = a + b;
+		
+		
+		System.out.println(a + "\n" + b + "\n" + "C: " + c);
+
+		
+	
+	}
+
+}
