2540e4a1558e13e7c9591ea278b5e9870e301d5b
HanssenGLRP <66432612+HanssenDevs@users.noreply.github.com>
2024-08-25 15:08:12 +0200

    Create OppgaveG3.java

---
+++ Fritid/DAT100Lab1/src/OppgaveG3.java
@@ -0,0 +1,15 @@
+
+public class OppgaveG3 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 2;
+		
+		if (b != 0) {
+			System.out.println("A/B = " + a/b);
+		} else {
+			System.out.println("Kan ikke utføre når b = 0");
+		}
+	}
+
+}
