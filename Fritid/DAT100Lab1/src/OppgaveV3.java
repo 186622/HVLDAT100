9b4028865512287609f4ca627f75d101e217bcbc
HanssenGLRP <66432612+HanssenDevs@users.noreply.github.com>
2024-08-27 21:59:26 +0200

    Create OppgaveV3.java

---
+++ Fritid/DAT100Lab1/src/OppgaveV3.java
@@ -0,0 +1,23 @@
+import static javax.swing.JOptionPane.*;
+
+public class OppgaveV3 {
+
+	public static void main(String[] args) {
+		String inputBrukerEn = showInputDialog("Skriv inn Heltall 1:");
+		String inputBrukerTo = showInputDialog("Skriv inn Heltall 2:");
+		String inputBrukerTre = showInputDialog("Skriv inn Heltall 3:");
+
+		int heltallEn = Integer.parseInt(inputBrukerEn);
+		int heltallTo = Integer.parseInt(inputBrukerTo);
+		int heltallTre = Integer.parseInt(inputBrukerTre);
+
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
