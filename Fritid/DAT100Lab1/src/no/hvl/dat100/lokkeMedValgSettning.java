8a6b05f5d00e62739490afba2bcf0775f79344fc
HanssenGLRP <66432612+HanssenDevs@users.noreply.github.com>
2024-08-28 18:57:05 +0200

    Update lokkeMedValgSettning.java

--- Fritid/DAT100Lab1/src/no/hvl/dat100/lokkeMedValgSettning.java
+++ Fritid/DAT100Lab1/src/no/hvl/dat100/lokkeMedValgSettning.java
@@ -5,36 +5,39 @@ public class lokkeMedValgSettning {
 
 	public static void main(String[] args) {
 		
-		
 		final int NEDRE = 0;
 		final int OVRE = 100;
 		
-		String melding = "Tenkt på tall mellom " + NEDRE + " - " + OVRE + ".";
-		
-		showMessageDialog(null, melding);
 		
 		int lav = NEDRE;
 		int hoy = OVRE;
 		int tall = 0;
 		boolean riktigSvar = false;
 		
+		String melding = "Tenk på et tall mellom " + NEDRE + " - " + OVRE + ".";
+		showMessageDialog(null, melding);
+		
 		do {
 			tall = (lav + hoy) / 2;
-			melding = "Tenker du på " + tall + "? " + "Hvis det er riktig, skriv 0, For høyt skriv -1, for Lavt skriv 1.";
+			melding = "Tenker du på tallet " + tall + "?" + "Hvis riktig, skriv 0. lav: 1. Hoyt: -1";
 			int svar = Integer.parseInt(showInputDialog(melding));
 			riktigSvar = (svar == 0);
 			
+			
 			if (!riktigSvar) {
-				if (svar == -1) {
-					hoy = tall -1;
-				} else {
+				if (svar == 1) {
 					lav = tall +1;
+					
+				} else {
+					hoy = tall -1;
 				}
 			}
 			
+			
 		} while (!riktigSvar);
-		
 		System.out.println("Gratulerer");
+		 
+		
 		
 	}
 	
