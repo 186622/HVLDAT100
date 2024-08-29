e6ab39cc5c98c9912fa241621370108c019a38d0
HanssenGLRP <66432612+HanssenDevs@users.noreply.github.com>
2024-08-29 14:57:41 +0200

    Create OppgaveG3uke36.java

---
+++ Fritid/DAT100Lab1/src/no/hvl/dat100/OppgaveG3uke36.java
@@ -0,0 +1,41 @@
+package no.hvl.dat100;
+
+public class OppgaveG3uke36 {
+
+	public static void main(String[] args) {
+		//a)
+		for (int i = 1; i<=20; i++) {
+			//b)
+			switch (i) {
+			case 1:
+				System.out.println("A");
+			break;
+			case 2:
+				System.out.println("B");
+			break;
+			default:
+				System.out.println("C");
+			}
+		
+			
+		}
+		//c)
+		for (int i = 1; i<=20; i++) {
+			if (i == 1) {
+				System.out.println("A");
+			} else if (i == 2) {
+				System.out.println("B");
+				
+			} else {
+				System.out.println("C");
+			}
+			
+		}
+		
+		
+		
+		
+		
+	}
+
+}
