abc747a2db311a19d3ae75947fa1b4bae86c3e1c
HanssenGLRP <66432612+HanssenDevs@users.noreply.github.com>
2024-08-29 16:19:13 +0200

    Update OppgaveB5uke36OBLI.java

--- Fritid/DAT100Lab1/src/no/hvl/dat100/OppgaveB5uke36OBLI.java
+++ Fritid/DAT100Lab1/src/no/hvl/dat100/OppgaveB5uke36OBLI.java
@@ -1,25 +1,46 @@
 package no.hvl.dat100;
+import static javax.swing.JOptionPane.*;
 
 public class OppgaveB5uke36OBLI {
 
 	public static void main(String[] args) {
 		
-		 int POENG = 20;
+
+		
+     int POENG = 100;	
+	final int ELEVER = 2;	
+	for (int i = 0; i <= ELEVER; i++) {	
+		String elevNavn = showInputDialog("Navn på Elev:");
+		String elevPoeng = showInputDialog("Poengsum til Elev:");
+		 POENG = Integer.parseInt(elevPoeng);
+		
+		 while (POENG < 0 || POENG > 100) {
+			 showMessageDialog(null, "ugyldig poengsum, skriv in på nytt");
+			  elevPoeng = showInputDialog("Poengsum til Elev:");
+			  POENG = Integer.parseInt(elevPoeng);
+		 }
+
+		 
 		if ( POENG >= 90 && POENG <= 100) {
-			System.out.println("Karakter: A");
+			System.out.println( elevNavn + " Hadde " + POENG + " poeng " + " og får Karakter: A");
 		} else if (POENG >= 80 && POENG <= 89) {
-			System.out.println("Karakter: B");
+			System.out.println( elevNavn + " Hadde " + POENG + " poeng " + " og får Karakter: B");
 		} else if (POENG >= 60 && POENG <=79) {
-			System.out.println("Karakter: C");
+			System.out.println( elevNavn + " Hadde " + POENG + " poeng " + " og får Karakter: C");
 		}else if (POENG >= 50 && POENG <=59) {
-			System.out.println("Karakter: D");
+			System.out.println( elevNavn + " Hadde " + POENG + " poeng " + " og får Karakter: D");
 		}else if (POENG >= 40 && POENG <=49) {
-			System.out.println("Karakter: E");
+			System.out.println( elevNavn + " Hadde " + POENG + " poeng " + " og får Karakter: E");
 		}else if (POENG >= 0 && POENG <=39) {
-			System.out.println("Karakter: F");
+			System.out.println( elevNavn + " Hadde " + POENG + " poeng " + " og får Karakter: F");
 		} else {
-			System.out.println("Ugyldig poengsum");
+			System.out.println("Ugyldig poengsum Skriv inn på nytt");
+		    elevNavn = showInputDialog("Navn på Elev:");
+			elevPoeng = showInputDialog("Poengsum til Elev:");
+
 		}
+	}
+
 
 		
 	}
