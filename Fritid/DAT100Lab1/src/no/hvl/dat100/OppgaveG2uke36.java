f30e5f0f2efa65a7f0be86d06afc3ed4a6de7f46
HanssenGLRP <66432612+HanssenDevs@users.noreply.github.com>
2024-08-29 14:48:39 +0200

    Create OppgaveG2uke36.java

---
+++ Fritid/DAT100Lab1/src/no/hvl/dat100/OppgaveG2uke36.java
@@ -0,0 +1,16 @@
+package no.hvl.dat100;
+
+public class OppgaveG2uke36 {
+
+	public static void main(String[] args) {
+		int i = 20;
+		while (i >=1) {
+			System.out.println(i);
+			i--;
+		}
+		
+		
+
+	}
+
+}
